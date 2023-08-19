1ddc2585268dc167fb273a7e126f84045bc9c720
thinhpham <phamthinh30522@gmail.com>
2023-08-18 23:44:53 -0700

    update code

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import {Sora} from "@next/font/google"
+
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+import TopLeftImg from "./TopLeftImg"
+import Nav from "./Nav"
+import Header from "./Header"
+
+
+const Layout = ({children}) => {
+  return <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}>
+    <TopLeftImg/>
+    <Nav/>
+    <Header/>
+    {children}
+  </div>
+};
+
+export default Layout;
---
+++ components/Nav.js
@@ -0,0 +1,45 @@
+// icons
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from 'react-icons/hi2';
+import Link from 'next/link'
+import {useRouter} from "next/router"
+
+// nav data
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'about', path: '/about', icon: <HiUser /> },
+  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  {
+    name: 'testimonials',
+    path: '/testimonials',
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: <HiEnvelope />,
+  },
+];
+
+const Nav = () => {
+  const router = useRouter()
+  return <nav className="fixed bottom-[20px] xl:w-[30%] lg:w-[40%] md:w-[50%] w-[90%] left-1/2 -translate-x-1/2 drop-shadow-2xl"> 
+    <div className="w-full rounded-full backdrop-blur-ms bg-white/30 px-2">
+    <ul className="flex items-center justify-between h-full">
+      {
+        navData.map((item) => <li key={item.name} className={`text-white hover:text-accent py-2 transition-all duration-300 rounded-full `}><Link href={item.path} className={`p-[12px] drop-shadow-2xl rounded-full block ${router.pathname === item.path && "text-accent bg-white" }`}><span className="text-lg">{item.icon}</span></Link></li>)
+      }
+
+    </ul>
+    </div>
+  </nav>;
+};
+
+export default Nav;
