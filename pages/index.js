4350b6624ce1947026d0ae63d6a3cff26fa8830a
thinhpham <phamthinh30522@gmail.com>
2023-08-19 00:01:38 -0700

    update code

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
+import Image from "next/image"
+
+//component 
+import ParticlesContainer from "./../components/ParticlesContainer"
+import ProjectBtn from "./../components/ProjectsBtn"
+import Avatar from "./../components/Avatar"
+
+//framer motion
+import motion from "framer-motion"
+
+//variants
+import {fadeIn} from "../variants"
+
 const Home = () => {
-  return <div>Home</div>;
+  return <div className="bg-primary/60 h-full">
+
+  </div>;
 };
 
 export default Home;
