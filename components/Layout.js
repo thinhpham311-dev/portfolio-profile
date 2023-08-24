2cda6f749419135afd468a3030698e2485fa263f
thinhpham <phamthinh30522@gmail.com>
2023-08-24 04:01:43 -0700

    update code

--- components/Layout.js
+++ components/Layout.js
@@ -14,7 +14,7 @@ import Circles from './Circles';
 
 const Layout = ({children}) => {
   return <div className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable}`}>
-    <div className=" bg-gradient-to-r from-primary/10 via-black/30 to-black/10 h-full content-between flex flex-col">
+    <div className="relative bg-gradient-to-r from-primary/10 via-black/30 to-black/10 h-full content-between flex flex-col">
     <TopLeftImg/>
     <Header/>
     {children}
