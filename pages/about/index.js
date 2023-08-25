f1166583629082f70de6cee1f878a118f82fc18d
thinhpham <phamthinh30522@gmail.com>
2023-08-25 01:04:12 -0700

    update code

--- pages/about/index.js
+++ pages/about/index.js
@@ -46,16 +46,16 @@ const aboutData = [
     title: 'experience',
     info: [
       {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
+        title: 'Front End Developer - PAP Technology company',
+        stage: '2020/06 - 2022/02',
       },
       {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
+        title: 'Web Developer - Antking Singapore Company',
+        stage: '2022/03 - 2022/08',
       },
       {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
+        title: 'Front End Developer - TechVSI company',
+        stage: '2022/11 - 2023/08',
       },
     ],
   },
@@ -83,18 +83,18 @@ const About = () => {
 
   return <div className="text-center xl:text-left xl:pt-32 py-0 h-full">
     <div className="container mx-auto px-5 h-full flex flex-col xl:items-start items-center xl:flex-row xl:gap-x-10 gap-x-4">
-      <div className="flex-1 flex flex-col justify-center">
+      <div className="flex flex-col justify-center h-[200px]">
         <h1 className="h1 xl:text-left">I'm <br /> <span className="text-accent">Web Developer</span></h1>
         <p className="xl:text-left w-full xl:max-w-[84%]"> I'm a Web Developer with my experience developing the website. I can use my knowledge and skills to increase aesthetics and increase user experience. </p>
       </div>
 
-      <div className='flex flex-col w-full xl:max-w-[48%] h-[480px] overflow-y-auto'>
-        <div className=" sticky bg-site text-white  top-0 left-0 h-[50px] mb-3">
-         <div className='bg-gradient-to-r h-full from-primary/10 via-black/30 to-black/10 flex items-center xl:justify-start justify-center xl:gap-x-8 lg:gap-x-6 gap-x-4'>
+      <div className='flex flex-col w-full xl:max-w-[48%] h-[calc(100%-200px)]'>
+        <div className="  text-white  h-[50px] mb-3 flex items-center xl:justify-start justify-center xl:gap-x-8 lg:gap-x-6 gap-x-4">
           {aboutData.map((item, indexItem) => <div key={indexItem} onClick={() => setIndex(indexItem)} className={`${index === indexItem && "text-accent after:!bg-accent after:w-full after:transition-all after:duration-300"} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}>{item.title}</div>)}
-         </div>
         </div>
+        <div className='h-[calc(100%-60px)] overflow-y-auto'>
           <div className=' flex flex-col gap-y-2 xl:gap-y-4'>
+
             {aboutData[index].info.map((item, indexItem) => <div key={indexItem} className='mx-auto xl:mx-0 flex-1 flex-col md:flex-row max-w-max gap-x-2 text-white/60'>
               <h3 className="capitalize mb-2">{item.title}</h3>
               <div className='text-white'>{item.stage}</div>
@@ -104,6 +104,7 @@ const About = () => {
                 }
               </div>
             </div>)}
+          </div>
         </div>
       </div>
     </div>
